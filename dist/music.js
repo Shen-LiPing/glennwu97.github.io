49d5ce0b8601d12d77d69d1937ab07bd6e54d59e
gerlan <347989530.qq.com>
2020-04-01 00:00:00 +0800

    Updated at 2020-03-31 23:59:52

--- dist/music.js
+++ dist/music.js
@@ -1,10 +1,16 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
+    container: document.getElementById('aplayer'),
+    fixed: true,
     mini: true,
-    audio: [{
-        name: 'name',
-        artist: 'artist',
-        url: 'url.mp3',
-        cover: 'cover.jpg',
-    }]
+   // autoplay: false,  //吸盘模式
+    audio: [
+	  {
+        name: 'Shelter',
+        artist: 'Shelter',
+        url: 'http://music.163.com/song/media/outer/url?id=461347460.mp3',
+        cover: '/img/favicon.png',
+      },
+     
+    ]
 });
+
