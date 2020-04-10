9fad4769f10cdbe55c5a268c3ec72fed614231d6
gerlan <347989530.qq.com>
2020-04-10 23:13:23 +0800

    Updated at 2020-04-10 23:13:09

--- aplayer_dist/music.js
+++ aplayer_dist/music.js
@@ -1,20 +1,3 @@
-// const ap = new APlayer({
-//     container: document.getElementById('aplayer'),
-//     fixed: true,
-//    //narrow: true,
-//    // autoplay: false,  //吸盘模式
-//     mini: true, //微型带图标模式
-//     audio: [
-// 	  {
-//         name: 'Shelter',//'Shelter'
-//         artist: 'Shelter' ,//'Shelter'
-//         url: 'https://music.163.com/song/media/outer/url?id=461347460.mp3',
-//         cover: '', //'/img/favicon.png',
-//       },
-     
-//     ]
-// });
-
 const ap = new APlayer({
   container: document.getElementById('aplayer'),
   autoplay: false,
@@ -22,6 +5,8 @@ const ap = new APlayer({
   //mini: true,
   fixed: true,
   width: '',
+  volume: 0.7,
+  mutex: true,
   loop: 'all',
   preload: 'auto', 
   theme: '#e9e9e9',
@@ -29,7 +14,7 @@ const ap = new APlayer({
       {
           name: 'Shelter (Piano version)',
           artist: 'Porter Robinson / Madeon',
-          url: 'https://music.163.com/song/media/outer/url?id=461347460.mp3',
+          url: 'https://music.163.com/#/song?id=427606780.mp3',
           cover: 'https://p2.music.126.net/nEaSbkg_Cn8gN9vLoSz0gQ==/18494885092595843.jpg',
           lrc: ""
       } 
